fc0952a1bb4e9bd5638cb921d9b4a55b8bfa370f
srikar reddy <srikarreddymadaka@gmail.com>
2022-03-21 20:19:28 +0530

    add spring starter projects for backend

---
+++ library-mysql/src/main/java/com/recasttech/mysql/LibraryMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.recasttech.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryMysqlApplication.class, args);
+	}
+
+}
---
+++ library-mysql/src/test/java/com/recasttech/mysql/LibraryMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recasttech.mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
